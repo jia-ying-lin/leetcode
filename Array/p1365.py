2d68127351661a5f3fc2f7c0f5c2e0636ba906cb
jiaying <jiaying@jiayings-MacBook-Pro.local>
2020-10-06 09:04:33 +0800

    add nlogn sol for p1365

--- Array/p1365.py
+++ Array/p1365.py
@@ -12,3 +12,12 @@ class Solution:
                     count += 1
             ans.append(count)
         return ans
+    '''
+    O(nlogn)
+    Big O of sorted is nlogn
+    '''
+    def smallerNumbersThanCurrent(self, nums: List[int]) -> List[int]:
+        count = {}
+        for i, n in enumerate(sorted(nums)):
+            count.setdefault(n, i)
+        return [count[num] for num in nums]
