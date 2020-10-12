574dab9968d7b8705e8f716fa950614eea47b5ce
jiaying <jiaying@jiayings-MacBook-Pro.local>
2020-10-12 09:26:12 +0800

    add sol for p1207 with collections

--- HashTable/p1207.py
+++ HashTable/p1207.py
@@ -11,3 +11,13 @@ class Solution:
             else:
                 h[i] = h[i] + 1
         return len(h.keys()) == len(set(h.values()))
+    """
+    Using collections https://docs.python.org/2/library/collections.html#module-collections
+    Runtime: 24 ms, faster than 99.31% of Python3 online submissions for Unique Number of Occurrences.
+    Memory Usage: 14.4 MB, less than 100.00% of Python3 online submissions for Unique Number of Occurrences.
+    """
+    class Solution:
+    def uniqueOccurrences(self, arr: List[int]) -> bool:
+        counts = collections.Counter(arr)
+        return len(counts.keys()) == len(set(counts.values()))
+        
